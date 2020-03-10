f66c282fd01f7a70c12aaca3411399c4dd1dba7f
Lloyd <lloyd.louw@mmiholdings.co.za>
2020-03-10 10:38:46 +0200

    Simple service impl code

--- soapservice-api/src/main/java/techbasics/api/soapservice/v1/service/SoapService.java
+++ soapservice-api/src/main/java/techbasics/api/soapservice/v1/service/SoapService.java
@@ -9,6 +9,6 @@ public interface SoapService {
     //This should be in a properties file to make provision for different environments.
     String NAMESPACE_URI_V1 = "http://localhost:8080/soapService/v1";
 
-    Response1 processSoapRequest1(Request1 request1);
+    Response1 doSomething(Request1 request1);
 
 }
--- soapservice-module/src/main/java/techbasics/domain/soapservice/service/v1/SoapserviceImpl.java
+++ soapservice-module/src/main/java/techbasics/domain/soapservice/service/v1/SoapserviceImpl.java
@@ -6,15 +6,26 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import techbasics.api.soapservice.v1.model.Request1;
 import techbasics.api.soapservice.v1.model.Response1;
 import techbasics.api.soapservice.v1.service.SoapService;
+import techbasics.mapper.Request1Mapper;
+import techbasics.mapper.Response1Mapper;
+
+import java.util.Collections;
 
 @Endpoint
 public class SoapserviceImpl implements SoapService {
 
 
-	@PayloadRoot(namespace = SoapService.NAMESPACE_URI_V1, localPart = "processSoapRequest1")
-	public Response1 processSoapRequest1(@RequestPayload Request1 request1) {
+	@PayloadRoot(namespace = SoapService.NAMESPACE_URI_V1, localPart = "doSomething")
+	public Response1 doSomething(@RequestPayload Request1 request1) {
 
-		return new Response1();
+		techbasics.domain.model.Request1 domainRequest = Request1Mapper.INSTANCE.mapToDomain(request1);
+
+		techbasics.domain.model.Response1 resp = new techbasics.domain.model.Response1();
+		resp.setResponseInt1(domainRequest.getRequestInt1()*2);
+		resp.setResponseString1("SoapServiceV1("+domainRequest.getRequestString1()+")");
+		resp.setResponse1Strings(Collections.singletonList(resp.getResponseString1()));
+
+		return Response1Mapper.INSTANCE.mapToApi(resp);
 
 	}
 
